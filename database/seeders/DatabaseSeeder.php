b3ba580ea6a0d0dff45b1cc17a0fe5bc119dd2c1
Developer <developer@laratenant.local>
2026-07-09 21:17:42 +0100

    Add PlansTableSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PermissionSeeder::class,
+            PlansTableSeeder::class,
             StoreSeeder::class,
             CategorySeeder::class,
             BrandSeeder::class,
